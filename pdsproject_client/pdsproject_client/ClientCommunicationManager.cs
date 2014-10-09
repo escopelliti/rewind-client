6d17b578ca3d31c3bae44738c66cfc53dfc1105a
acannavo <cann.alberto91@gmail.com>
2014-10-09 15:39:48 +0200

    Prime classi

---
+++ pdsproject_client/pdsproject_client/ClientCommunicationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace CommunicationLibrary
+{
+    class ClientCommunicationManager : ClientServerCommunicationManager
+    {
+        public Socket Connect(string host, int port, Socket socket)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(host);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            socket.Connect(remoteEP);
+            return socket;
+        }
+    }
+}
---
+++ pdsproject_client/pdsproject_client/ClientServerCommunicationManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CommunicationLibrary
+{
+    class ClientServerCommunicationManager
+    {
+        public Socket CreateSocket(ProtocolType protocolType)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, protocolType);
+            return socket;
+        }
+
+        public void Send(byte[] toSend, Socket socket)
+        {
+            socket.Send(toSend);
+        }
+
+        public void SendObject(object toSend)
+        {
+            //Serialization and then send
+            //clientSocket.BeginSend(toSend, 0, toSend.Length, 0, sendHandler, clientSocket);
+        }
+
+        public void SendFiles(List<string> filepathList, Socket socket)
+        {
+            foreach (string fileName in filepathList)
+            {
+                socket.SendFile(fileName);
+            }
+        }
+
+        public int Receive(byte[] bytes, Socket socket)
+        {
+            return socket.Receive(bytes);
+        }
+
+        private void ReceiveObject()
+        {
+
+            //deserializzazione
+        }
+        //public struct SocketObject
+        //{
+        //    public Socket socket;
+        //    public const int bufferSize = 1024;
+        //    public byte[] receiveBuffer;
+        //}
+    }
+}
