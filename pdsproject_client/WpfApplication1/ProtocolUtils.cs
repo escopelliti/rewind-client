3468828418d0a9fb6349827cc3618c232db68c91
escopelliti <en.scopelliti@gmail.com>
2014-11-06 15:20:34 +0100

    First import of function for moving files between server and client.

--- pdsproject_client/WpfApplication1/ProtocolUtils.cs
+++ pdsproject_client/WpfApplication1/ProtocolUtils.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Concurrent;
 
-namespace ServerTest
+namespace Protocol
 {
     static class ProtocolUtils
     {
+        public static ConcurrentDictionary<string, string> protocolDictionary = new ConcurrentDictionary<string, string>();
 
         public const string REQUEST = "request";
         public const string CONTENT = "content";
@@ -16,9 +19,28 @@ namespace ServerTest
         public const string NAME = "name";
         public const string SIZE = "size";
 
-        public struct FileStruct {
+        public const string SET_CLIPBOARD_TEXT = "CLIPBOARD_TEXT";
+        public const string SET_CLIPBOARD_FILES = "CLIPBOARD_FILES";
+        public const string SET_CLIPBOARD_IMAGE = "CLIPBOARD_IMAGE";
+        public const string TRANSFER_FILES = "BEGIN_TRANSFER_FILES";
+        public const string TRANSFER_IMAGE = "BEGIN_TRANSFER_IMAGE";
+
+        public const string TMP_IMAGE_FILE = ".\\tmp.jpg";
+        public const string TMP_DIR = ".\\tmp\\";
+
+        public struct FileStruct
+        {
             public string name;
             public long size;
+            public string dir;
+        }
+
+        public static void InitProtocolDictionary()
+        {
+            protocolDictionary[SET_CLIPBOARD_TEXT] = SET_CLIPBOARD_TEXT;
+            protocolDictionary[SET_CLIPBOARD_FILES] = TRANSFER_FILES;
+            protocolDictionary[SET_CLIPBOARD_IMAGE] = TRANSFER_IMAGE;
+            protocolDictionary[TRANSFER_IMAGE] = TMP_IMAGE_FILE;
         }
     }
 }
