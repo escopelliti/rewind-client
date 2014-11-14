5d104fc2130f9b21298074b84c5f947770c644a2
escopelliti <en.scopelliti@gmail.com>
2014-11-14 19:45:10 +0100

    some fix somewhere

--- pdsproject_client/WpfApplication1/ComputerItem.cs
+++ pdsproject_client/WpfApplication1/ComputerItem.cs
@@ -9,17 +9,8 @@ namespace WpfApplication1
     public class ComputerItem
     {
         public String Name { get; set; }
-        public String ComputerStateImage { get; set; }
-        public String focusedImage { get; set; }
-        public State computerState { get; set; }
+        public String ComputerStateImage { get; set; }                
         public int computerNum { get; set; }
         public int computerID { get; set; }
-    }
-
-    public class State
-    {
-        public const string ACTIVE = "ACTIVE";
-        public const string CONNECTED = "CONNECTED";
-        public const string DISCONNECTED = "DISCONNECTED";
     }
 }
